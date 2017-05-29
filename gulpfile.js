1ee1e07e70236901964505c92092dfbddf38a26c
Diego de Araujo Silva <araujo.die93@gmail.com>
2017-05-29 20:13:58 -0300

    Criada a tarefa para minimizar imagens

--- gulpfile.js
+++ gulpfile.js
@@ -5,4 +5,10 @@ var gulp = require("gulp"),
 // criação de uma tarefa
 gulp.task("minify-img", function(){
 
+	// seleciona os arquivos
+	gulp.src("src/img/**/*")
+		// realiza a minificação das imagens
+		.pipe(imagemin())
+		// salva os arquivos minificados na pasta apontada
+		.pipe(gulp.dest("src/img"));
 });
