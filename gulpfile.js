33bfd573427990313e85c0f53ab2f14f4301e697
Diego de Araujo Silva <araujo.die93@gmail.com>
2017-05-29 20:44:36 -0300

    Tarefa copy adicionada

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,13 @@
 var gulp = require("gulp"),
 	imagemin = require("gulp-imagemin");
 
+// copia os arquivos da fonte para distribuição
+gulp.task("copy", function(){
+	gulp.src("src/**/*")
+		.pipe(gulp.dest("dist"));
+
+});
+
 // criação de uma tarefa
 gulp.task("minify-img", function(){
 
